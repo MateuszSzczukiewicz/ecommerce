9e5c9050ddfc0333f5121e400588fc98e0108f3a
MateuszSzczukiewicz <szcmat6277@gmail.com>
2024-07-30 21:14:28 +0200

    Added generic routes and active class name

--- src/ui/atoms/ActiveLink.tsx
+++ src/ui/atoms/ActiveLink.tsx
@@ -1,18 +1,28 @@
 "use client";
 import Link from "next/link";
+import { type ReactNode } from "react";
 import clsx from "clsx";
 import { usePathname } from "next/navigation";
-import { type ReactNode } from "react";
+import { type Route } from "next";
 
-export const ActiveLink = ({ href, children }: { href: string; children: ReactNode }) => {
+type ActiveLinkProps<T extends string> = {
+	href: Route<T>;
+	children: ReactNode;
+};
+
+export const ActiveLink = <T extends string>({ href, children }: ActiveLinkProps<T>) => {
 	const pathname = usePathname();
+
 	const isActive = pathname === href;
+	const activeClassName = `text-zinc-950 font-semibold text-xl`;
+
 	return (
 		<Link
 			href={{ pathname: href }}
 			className={clsx(`h-full text-lg text-zinc-600 hover:text-zinc-950`, {
-				underline: isActive,
+				[activeClassName]: isActive,
 			})}
+			aria-current="page"
 		>
 			{children}
 		</Link>
