f94f5a8b0ac39ea265c135802185c5eb1534249a
MateuszSzczukiewicz <szcmat6277@gmail.com>
2024-08-12 21:34:15 +0200

    Added cart mutations

--- src/gql/gql.ts
+++ src/gql/gql.ts
@@ -3,76 +3,7 @@ import * as types from './graphql';
 
 
 
-/**
- * Map of all GraphQL operations in the project.
- *
- * This map has several performance disadvantages:
- * 1. It is not tree-shakeable, so it will include all operations in the project.
- * 2. It is not minifiable, so the string of a GraphQL query will be multiple times inside the bundle.
- * 3. It does not support dead code elimination, so it will add unused operations.
- *
- * Therefore it is highly recommended to use the babel or swc plugin for production.
- */
-const documents = {
-    "query CategoriesGetList {\n  categories {\n    data {\n      ...CategoriesListItem\n    }\n  }\n}": types.CategoriesGetListDocument,
-    "query CategoriesGetProducts($slug: String!) {\n  category(slug: $slug) {\n    products {\n      ...ProductListItem\n    }\n  }\n}": types.CategoriesGetProductsDocument,
-    "fragment CategoriesListItem on Category {\n  name\n  slug\n}": types.CategoriesListItemFragmentDoc,
-    "fragment CollectionListItem on Collection {\n  id\n  name\n  slug\n  description\n  products {\n    ...ProductListItem\n  }\n}": types.CollectionListItemFragmentDoc,
-    "query CollectionsGetList {\n  collections {\n    data {\n      ...CollectionListItem\n    }\n  }\n}": types.CollectionsGetListDocument,
-    "query CollectionsGetProducts($slug: String!) {\n  collection(slug: $slug) {\n    products {\n      ...ProductListItem\n    }\n  }\n}": types.CollectionsGetProductsDocument,
-    "query ProductGetById($id: ID!) {\n  product(id: $id) {\n    ...ProductListItem\n  }\n}": types.ProductGetByIdDocument,
-    "query ProductsGetList($take: Int, $skip: Int) {\n  products(take: $take, skip: $skip) {\n    data {\n      ...ProductListItem\n    }\n    meta {\n      total\n    }\n  }\n}": types.ProductsGetListDocument,
-    "fragment ProductListItem on Product {\n  id\n  name\n  description\n  price\n  categories {\n    name\n    slug\n  }\n  collections {\n    name\n    slug\n  }\n  images {\n    url\n    alt\n    width\n    height\n  }\n}": types.ProductListItemFragmentDoc,
-    "query ProductsGetSuggested($take: Int, $order: SortDirection, $orderBy: ProductSortBy) {\n  products(take: $take, order: $order, orderBy: $orderBy) {\n    data {\n      ...ProductListItem\n    }\n  }\n}": types.ProductsGetSuggestedDocument,
-    "query ProductsSearchList($take: Int, $skip: Int, $searchTerm: String!) {\n  products(take: $take, skip: $skip, search: $searchTerm) {\n    data {\n      ...ProductListItem\n    }\n    meta {\n      total\n    }\n  }\n}": types.ProductsSearchListDocument,
-};
-
-/**
- * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
- */
-export function graphql(source: "query CategoriesGetList {\n  categories {\n    data {\n      ...CategoriesListItem\n    }\n  }\n}"): typeof import('./graphql').CategoriesGetListDocument;
-/**
- * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
- */
-export function graphql(source: "query CategoriesGetProducts($slug: String!) {\n  category(slug: $slug) {\n    products {\n      ...ProductListItem\n    }\n  }\n}"): typeof import('./graphql').CategoriesGetProductsDocument;
-/**
- * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
- */
-export function graphql(source: "fragment CategoriesListItem on Category {\n  name\n  slug\n}"): typeof import('./graphql').CategoriesListItemFragmentDoc;
-/**
- * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
- */
-export function graphql(source: "fragment CollectionListItem on Collection {\n  id\n  name\n  slug\n  description\n  products {\n    ...ProductListItem\n  }\n}"): typeof import('./graphql').CollectionListItemFragmentDoc;
-/**
- * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
- */
-export function graphql(source: "query CollectionsGetList {\n  collections {\n    data {\n      ...CollectionListItem\n    }\n  }\n}"): typeof import('./graphql').CollectionsGetListDocument;
-/**
- * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
- */
-export function graphql(source: "query CollectionsGetProducts($slug: String!) {\n  collection(slug: $slug) {\n    products {\n      ...ProductListItem\n    }\n  }\n}"): typeof import('./graphql').CollectionsGetProductsDocument;
-/**
- * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
- */
-export function graphql(source: "query ProductGetById($id: ID!) {\n  product(id: $id) {\n    ...ProductListItem\n  }\n}"): typeof import('./graphql').ProductGetByIdDocument;
-/**
- * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
- */
-export function graphql(source: "query ProductsGetList($take: Int, $skip: Int) {\n  products(take: $take, skip: $skip) {\n    data {\n      ...ProductListItem\n    }\n    meta {\n      total\n    }\n  }\n}"): typeof import('./graphql').ProductsGetListDocument;
-/**
- * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
- */
-export function graphql(source: "fragment ProductListItem on Product {\n  id\n  name\n  description\n  price\n  categories {\n    name\n    slug\n  }\n  collections {\n    name\n    slug\n  }\n  images {\n    url\n    alt\n    width\n    height\n  }\n}"): typeof import('./graphql').ProductListItemFragmentDoc;
-/**
- * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
- */
-export function graphql(source: "query ProductsGetSuggested($take: Int, $order: SortDirection, $orderBy: ProductSortBy) {\n  products(take: $take, order: $order, orderBy: $orderBy) {\n    data {\n      ...ProductListItem\n    }\n  }\n}"): typeof import('./graphql').ProductsGetSuggestedDocument;
-/**
- * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
- */
-export function graphql(source: "query ProductsSearchList($take: Int, $skip: Int, $searchTerm: String!) {\n  products(take: $take, skip: $skip, search: $searchTerm) {\n    data {\n      ...ProductListItem\n    }\n    meta {\n      total\n    }\n  }\n}"): typeof import('./graphql').ProductsSearchListDocument;
-
-
+const documents = {}
 export function graphql(source: string) {
   return (documents as any)[source] ?? {};
 }
--- src/ui/atoms/ProductDetails.tsx
+++ src/ui/atoms/ProductDetails.tsx
@@ -1,17 +1,78 @@
 import { type FC } from "react";
 import { formatMoney } from "@/app/utils";
-import { type ProductListItemFragment } from "@/gql/graphql";
+import { CartGetByIdDocument, type ProductListItemFragment } from "@/gql/graphql";
+import { cookies } from "next/headers";
+import { executeGraphql } from "@/api/graphqlApi";
 
 type ProductDetailsProps = {
 	product: ProductListItemFragment;
 };
 
 export const ProductDetails: FC<ProductDetailsProps> = ({ product }) => {
+	async function addProductToCartAction(formData: FormData) {
+		"use  server";
+		const cart = await getOrCreateCart();
+        cookies.().set("cartId", cart.id, {
+            httpOnly: true,
+            sameSite: "lax",
+            // secure: true
+        });
+		await addToCart(cart.id, product.productId);
+	}
+
 	return (
 		<div className="flex w-1/3 flex-col gap-5">
 			<h1 className="text-4xl font-bold">{product.name}</h1>
 			<p className="text-xl font-semibold">{formatMoney(product.price / 100)}</p>
 			<p className="text-lg">{product.description}</p>
+			<form action={addProductToCartAction}>
+				<input type="hidden" name="productId" value={product.id} />
+				<button
+					type="submit"
+					className="w-full rounded-md border bg-slate-700 px-8 py-3 text-white"
+				>
+					Add to cart
+				</button>
+			</form>
 		</div>
 	);
 };
+
+async function getOrCreateCart(): Promise<CartFragment> {
+	const cartId = cookies().get("cartId")?.value;
+	if (cartId) {
+		const cart = await getCartById(cartId);
+		if (cart.order) {
+			return cart.order;
+		}
+	}
+
+	const cart = await createCart();
+	if (!cart.createOrder) {
+		throw new Error("Failed to create cart");
+	}
+	return cart.createOrder;
+}
+
+async function getCartById(cartId: string) {
+	executeGraphql(CartGetByIdDocument, { id: cartId });
+}
+
+async function createCart() {
+	return executeGraphql(CartCreateDocument, {});
+}
+
+function addToCart(orderId: string, productId: string) {
+    const product = await executeGraphql(ProductGetById, {
+        id: productId
+    })
+    if(!product.product) {
+        throw new Error("Product not found")
+    }
+
+    await executeGraphql(CartAddProductDocument, {
+        orderId,
+        productId,
+        total: product.product.price
+    });
+}
