e87ef10b4d2f75ad076da1469f7fbf631546fa63
MateuszSzczukiewicz <szcmat6277@gmail.com>
2024-08-24 13:51:36 +0200

    Add POST route for cart API calls

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,3 +1,19 @@
-import { NextRequest } from "next/server";
+import { revalidatePath } from "next/cache";
+import { type NextRequest } from "next/server";
 
-// export async function GET(request: NextRequest): Promise<Response> {}
+export async function POST(request: NextRequest): Promise<Response> {
+	const body: unknown = await request.json();
+
+	if (
+		typeof body === "object" &&
+		body &&
+		"productId" in body &&
+		typeof body.productId === "string"
+	) {
+		revalidatePath(`/product/${body.productId}`);
+		revalidatePath(`/products`);
+		return new Response(null, { status: 204 });
+	} else {
+		return new Response(null, { status: 400 });
+	}
+}
